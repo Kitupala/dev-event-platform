167cc128da4cf262ccca0e673f9d242e664b0da6
Kitupala <kimmo.tuupanen@gmail.com>
2025-12-18 13:14:11 +0200

    Implement event details page, add EventDetails and BookEvent components

---
+++ components/BookEvent.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+
+const BookEvent = () => {
+  const [email, setEmail] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 1000);
+  };
+
+  return (
+    <div id="book-event">
+      {submitted ? (
+        <p className="text-sm">Thank you for signing up!</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email address"
+            />
+            <button type="submit" className="button-submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default BookEvent;
